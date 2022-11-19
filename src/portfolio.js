de3af1af9c18499fbb93c613b436b6a0b19611fc
siujaiden <jaidensiu@gmail.com>
2022-11-19 00:40:03 -0800

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -3,17 +3,17 @@ import aboutpic from "./components/Access/mePhoto.jpg"
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://jaidensiu.github.io/',
-  title: 'JS.',
+  title: 'Jaiden Siu',
 }
 
 const about = {
-  photo: aboutpic,
+  // photo: aboutpic,
   // all the properties are optional - can be left empty or deleted
   name: 'Jaiden Siu',
-  role: 'Engineering Student',
+  role: 'aspiring engineer',
   description:
-    'UBC Biomedical Engineering Co-op Student who loves mathematics, computer science, and the intersection of medical technology.',
-  resume: 'https://drive.google.com/drive/folders/12MP3uHk94Ew6_egSJqgxbvycI60gHfXA?usp=sharing',
+    'UBC Biomedical Engineering Co-op Student who loves mathematics, computer science, and the intersection of business and technology.',
+  resume: 'https://drive.google.com/file/d/1cn70AwxVaB66qTCee8dvBSdD1yMSZZ67/view?usp=sharing',
   social: {
     linkedin: 'https://www.linkedin.com/in/jaidensiu/',
     github: 'https://github.com/jaidensiu',
@@ -24,44 +24,28 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Graphs, Games, and Interplanetary Travel',
     description:
-      'As users, we see a page that lets anyone look for the movies from the search bar by key words, and obtaining the actual or similar movies with their years, actors, rating, and description information by recommendation. In addition, they can search the nearby theaters from where they currently are located.',
-    stack: ['localstorage', 'Openweather API', 'Google Map API','Movie Database API'],
-    sourceCode: 'https://github.com/Mike-C-Sanders/Movie-Tracker',
-    livePreview: 'https://mike-c-sanders.github.io/Movie-Tracker/',
+      'A  UBC CPEN 221 all about graphs and its algorithms. From designing adjacency matrix and list representations to leveraging novel graph traversal algorithms, this project certainly taught me a lot about graphs. This project was done in Java with development tools such as Gradle, IntelliJ and JaCoCo.',
+    stack: ['Java', 'Gradle', 'IntelliJ', 'JaCoCo','JUnit'],
+    sourceCode: '',
+    livePreview: '',
   },
   {
-    name: 'Project 2',
+    name: 'n-grams, Autocompletion, and Gender Bias',
     description:
-      'Event Planner was designed with the partier in mind, the one who wants to find fine dinning or a friend to have a good time. Event Planner is the easiest way for anyone to find an event to attend, create or host your own event or browse whats happening in your local area.',
-    stack: ['swiper.js', 'npm', 'jawsdb','handlebars','express','nodejs'],
-    sourceCode: 'https://github.com/Morganbb104/Event_Planner_project2',
-    livePreview: 'https://obscure-chamber-98928.herokuapp.com/',
+      'A UBC CPEN 221 all about basic notions of natural language processing (and good software engineering practices, of course). I was involved with implementing parsing algorithms using regular expressions to construct n-grams obtained from large data sets containing texts.',
+    stack: ['Java', 'Gradle', 'IntelliJ', 'JUnit'],
+    sourceCode: '',
+    livePreview: '',
   },
   {
-    name: 'Resource Planner',
+    name: 'Cell Simulation and Automata',
     description:
-      'Resource Planner is able to distrute the current resource including human resource,time and budget in one company in order to help user to manahe their projects',
-    stack: ['html-css-javascript', 'sequelize', 'React'],
-    sourceCode: 'https://github.com/stone-com/Resource-Planner',
-    livePreview: 'https://resource-planner-group2.herokuapp.com/',
-  },
-  {
-    name: 'GameStoreData(back end)',
-    description:
-      'GameStoreData is able to let user retrieve the game from database based on its year and category',
-    stack: ['Java 8', 'MySQL', 'React'],
-    sourceCode: 'https://github.com/stone-com/Resource-Planner',
-    
-  },
-  {
-    name: 'Music Store(back end)',
-    description:
-      'MusicStore is able to direct user to the desired page based on its endpoints by track, album,artist and label',
-    stack: ['html-css-javascript', 'sequelize', 'React'],
-    sourceCode: 'https://github.com/Morganbb104/M7-project2-musictore-recommendations-Huang_Chengchien',
-    livePreview: 'https://cheng32-musicstore-catalog.herokuapp.com/track',
+      'A cellular automata all about visualizing cell interactions on a GUI which is commonly used for guiding therapeutics and deriving diagnostic insights when practicing medicine and research. This project is a great exhibition of the power that software systems can be utilized in medicine.',
+    stack: ['Java', 'Java AWT'],
+    sourceCode: '',
+    livePreview: '',
   },
 ]
 
@@ -71,21 +55,12 @@ const skills = [
   'HTML',
   'CSS',
   'JavaScript',
-  'TypeScript',
-  'Express',
-  'MySQL',
-  'MongoDB',
+  'SQL',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
   'Git',
-  'MERN',
-  'Java 8',
-  'Java Spring Boot',
-  'Microservices',
-  'Docker',
-  'Heroku'
+  'Java',
+  'C',
+  'C++'
 ]
 
 const contact = {
