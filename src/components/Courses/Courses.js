06f7d9177df0d994967f798a6b0b07172812bfbb
siujaiden <jaidensiu@gmail.com>
2022-12-05 17:19:47 -0800

    changed grid property for courses

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -7,10 +7,10 @@ const Courses = () => {
   if (!courses.length) return null
 
   return (
-    <section id='courses' className='section courses'>
+    <section id='courses' className='section'>
       <h2 className='section__title'>Relevant Courses</h2>
 
-      <div className='courses_grid'>
+      <div className='projects__grid'>
         {courses.map((course) => (
           <ProjectContainer key={uniqid()} project={course} />
         ))}
--- src/portfolio.js
+++ src/portfolio.js
@@ -13,7 +13,7 @@ const about = {
   name: 'Jaiden',
   role: '',
   description:
-    'UBC Biomedical Engineering Student who is interested in mathematics, computer science, and the intersection of business and technology.',
+    'I am a biomedical engineering student at UBC interested in mathematics, computer science, and the integration of business and technology.',
   resume: 'https://drive.google.com/file/d/1UFj3vidKyJUOdRvIeK83f2IHrfKvlN3I/view?usp=sharing',
   social: {
     linkedin: 'https://www.linkedin.com/in/jaidensiu/',
@@ -65,7 +65,7 @@ const courses = [
       'BMEG 220 is a course that is combined with MATH 264 Vector Calculus to deepen the knowledge of electromagnetics with vector calculus and introduces mathematical abstraction of electrical circuits. This is a very fast-paced two-in-one course that serves to lay the theoretical foundation for students to start conceptualizing medical instrumentation. Simulation software like MultiSim and MATLAB used to model circuits and electromagnetics, respectively, are also introduced. Overall a great course that covers so many interesting concepts.',
     stack: [''],
     sourceCode: '',
-    livePreview: '',
+    livePreview: 'https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-course&dept=BMEG&course=220',
   },
   {
     name: 'BMEG 371 Transport Phenomena',
@@ -73,7 +73,7 @@ const courses = [
       'Super interesting course that is meant to equip one with many mathematical tools to model biological systems (and many other cool things in the engineering sciences). Some of the topics discussed include transport of molecules, Brownian motion, diffusion-limited chemical rates, optimal transport, entropic regularization, Navier-Stokes equations, mass transport, and developmental stochastic processes. This course matured my mathematical abilities and introduced me to some advanced concepts in mathematics I once would have been intimidated of.',
     stack: [''],
     sourceCode: '',
-    livePreview: '',
+    livePreview: 'https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-course&dept=BMEG&course=371',
   },
   {
     name: 'COMR 473 Business Finance',
@@ -81,7 +81,7 @@ const courses = [
       'An introductory course in finance that overviews the corporate financial system. This course introduces concepts of time value of money, long-term securities like bonds, risky securities like stocks, and processes in which returns and values of assets relate to eachother. Very interesting to see the theory of this course apply to capital allocation decisions and in determining capital structure of a firm. Overall was an interesting course in applying simple mathematical and statistical idea to model complex financial phenomena.',
     stack: [''],
     sourceCode: '',
-    livePreview: '',
+    livePreview: 'https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-course&dept=COMR&course=473',
   },
   {
     name: 'CPEN 221 Software Construction',
@@ -89,7 +89,7 @@ const courses = [
       'This is a super challenging course that covers many of the first principles when it comes to constructing software from a theoretical and practical perspective. The course covers core topics such as static checking, data abstractions, procedural specifications, testing, exceptions, mutability, abstract data types, subtypes, recursive types, streams, grammars, regular expressions, concurrency, and parallelism. Beyond the core topics, one is exposed to fundamental data structures and algorithms, a taste of algorithmic analysis, design patterns, random testing, formal verification, and dynamic programming. This course has really changed the way I view software and I am beyond grateful to have had the opportunity to enroll in such a fantastic course filled with passionate individuals in the teaching team.',
     stack: [''],
     sourceCode: '',
-    livePreview: '',
+    livePreview: 'https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-course&dept=CPEN&course=221',
   },
   {
     name: 'ELEC 221 Signals and Systems',
@@ -97,7 +97,7 @@ const courses = [
       'Interesting course that introduces the processing and analysis of signals and systems, a vast topic that has many applications across the engineering sciences. Topics include convolution, Fourier series, filtering, Fourier transforms, sampling, communication systems, modulation, Laplace transforms, Z-transforms, and feedback systems. Computational tools such as Python and NumPy are also introduced to reinforce mathematical concepts algorithmically. Overall, this course challenged and equipped me with the set of tools in the modeling of systems and the processing of signals that abstract audio and images.',
     stack: [''],
     sourceCode: '',
-    livePreview: '',
+    livePreview: 'https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-course&dept=ELEC&course=221',
   },
   {
     name: 'MATH 220 Mathematical Proof',
@@ -105,7 +105,7 @@ const courses = [
       'A course that rigorously challenges one to think of all mathematics at a very constructive level. MATH 220 taught me to be very patient and dissect mathematical definitions to their core, which has allowed me to carry such way of thinking to other courses such as CPEN 221 and BMEG 371. This is definitely a course that has developed me intellectually and truly challenged first principles thinking.',
     stack: [''],
     sourceCode: '',
-    livePreview: '',
+    livePreview: 'https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-course&dept=MATH&course=220',
   },
 ]
 
