5698e3be54167c6a7405f403f4b0b683577f05fc
siujaiden <jaidensiu@gmail.com>
2023-04-04 16:00:13 -0700

    uddate about description

--- src/portfolio.js
+++ src/portfolio.js
@@ -13,7 +13,7 @@ const about = {
   name: 'Jaiden',
   role: '',
   description:
-    'Welcome to my personal website portfolio! I\'m currently a third year Biomedical Engineering student at the University of British Columbia (UBC) specializing in Software and Informatics Engineering, and minoring in Commerce — it\'s quite a mouthful! Outside of school and work, I enjoy working out, swimming, and playing the piano. I\'m currently looking for software co-op/internship opportunities starting in the fall 2023.',
+    'Welcome to my personal website portfolio! I\'m currently a third year Engineering student at the University of British Columbia (UBC) specializing in Software Engineering. Outside of school and work, I enjoy working out, swimming, and playing the piano. I\'m currently looking for software co-op/internship opportunities starting in the fall 2023.',
   resume: '',
   social: {
     linkedin: 'https://www.linkedin.com/in/jaidensiu/',
