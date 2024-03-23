d9a661156a84a0236ae991d6fde6e49a4cccfd9c
Jaiden Siu <jaidensiu@gmail.com>
2024-03-22 18:02:24 -0700

    chore: fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,11 @@
 import './Footer.css'
 
 const Footer = () => (
-  <footer className='footer'>
-    <a href='/' className='link footer__link'>
-      © Jaiden Siu 2024
-    </a>
-  </footer>
+    <footer className='footer'>
+        <a href='/' className='link footer__link'>
+          © Jaiden Siu 2024
+        </a>
+    </footer>
 )
 
 export default Footer
