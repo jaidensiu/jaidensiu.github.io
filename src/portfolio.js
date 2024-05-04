b1427a48b610a8e0357bb63e96626c01be0137e9
siujaiden <jaidensiu@gmail.com>
2024-05-04 02:25:43 -0700

    chore: updating info

--- src/portfolio.js
+++ src/portfolio.js
@@ -8,9 +8,9 @@ const header = {
 const about = {
     photo: aboutpic,
     name: 'Jaiden',
-    role: 'Software Developer Co-op @ Avanti • BME + CS @ UBC',
+    role: 'Android Developer Co-op @ FORM • BME + CS @ UBC',
     description: 
-        'I\'m currently a student at the University of British Columbia studying Biomedical Engineering with a specialization in Computer Science. Passionate about architecting and developing software, my mission is to bring applications and services to fruition. In my free time, you\'ll find me breaking a sweat at the gym, conquering new heights at the climbing gym, and bringing my engineering projects to life.',
+        'I\'m currently a student at the University of British Columbia studying Biomedical Engineering with a specialization in Computer Science. Passionate about architecting and developing software, my mission is to bring applications and services to fruition. In my free time, you\'ll find me breaking a sweat at the gym, refining my swimming technique at the pool, and bringing my engineering projects to life.',
     contact: {
         // resume: 'https://drive.google.com/file/d/1kRbtdnIrpz4b0WgfL3cbeu8xqBdReFm-/view?usp=sharing',
         github: 'https://github.com/jaidensiu',
@@ -24,22 +24,24 @@ const experiences = [
         company: 'Form Athletica Inc.',
         companyLink: 'https://ca.formswim.com/',
         role: 'Android Developer Co-op',
-        date: 'Incoming Apr 2024',
-        description: String().concat(),
-        stack: []
+        date: 'Apr 2024 - Present',
+        description: String().concat(
+            'Developing and testing the FORM Swim app for Android to provide a seamless user experience\n',
+            'Researching and architecting existing features to improve app performance and code maintainability'
+        ),
+        stack: ['Kotlin', 'XML', 'RxAndroid/RxJava', 'Room', 'Protobuf', 'Android Studio', 'Jenkins', 'Bluetooth Low Energy (BLE)']
     },
     {
         company: 'Avanti Software Inc.',
         companyLink: 'https://www.avanti.ca/',
         role: 'Software Developer Co-op',
-        date: 'Sep 2023 - Present',
+        date: 'Sep 2023 - Apr 2024',
         description: String().concat(
-            'Building a mobile app for HR management with 18000+ users to enhance user engagement and satisfaction\n',
-            'Working in an agile scrum team to develop, review, and deploy production-level code for Android and iOS\n',
-            'Implementing Figma designs and building business logic to present the UI in Kotlin using Jetpack Compose\n',
-            'Applying a MVVM pattern to modularize API calls and UI states with Dagger and Hilt for dependency injection\n',
-            'Developing a repository layer to consume a REST API using Retrofit and OkHttp, and cache data using Room\n',
-            'Writing unit tests to yield 95% line coverage using JUnit and Mockito to ensure code quality and correctness'
+            'Spearheaded the development of 10 new Android features for Avanti Go mobile app integrated with Firebase\n',
+            'Implemented Figma designs and building business logic to present the UI in Kotlin using Jetpack Compose\n',
+            'Applied a MVVM pattern to modularize API calls and UI states with Dagger and Hilt for dependency injection\n',
+            'Developed a repository layer to consume a REST API using Retrofit and OkHttp, and cache data using Room\n',
+            'Wrote unit tests to yield 95% line coverage using JUnit and Mockito to ensure code quality and correctness'
         ),
         stack: ['Kotlin', 'Jetpack Compose', 'XML', 'Retrofit', 'OkHttp', 'Room', 'JUnit', 'Mockito', 'Android Studio', 'Postman']
     },
@@ -61,9 +63,19 @@ const experiences = [
 
 const projects = [
     {
+        name: 'Gopher Banking Service',
+        description: String().concat(
+            'Developing a backend web service in Go to manage accounts, record transfers, and perform transactions\n',
+            'Implementing database transactions from SQL for reliable and concurrent interactions with PostgreSQL\n',
+            'Building a frontend using Vue.js and Axios to enable account management and transfers via API endpoints',
+        ),
+        stack: ['Go', 'SQL', 'PostgreSQL', 'Docker', 'GoLand'],
+        github: 'https://github.com/jaidensiu/gopher-banking-service'
+    },
+    {
         name: 'Noodle Doodle - 1st Place at nwHacks 2024',
         description: String().concat(
-            'Built an IoT device that enables users to customize and cook noodles automatically through an iOS app\n',
+            'Led the development of an IoT device that enables users to cook noodles automatically with an iPhone\n',
             'Implemented an iOS app in Swift using SwiftUI for user interfaces and Alamofire to handle HTTP connections\n',
             'Developed a Flask server in Python hosted on a Raspberry Pi to expose an API endpoint to the iOS client\n',
             'Actuated 4 servo motors from a Flask server using pigpio to execute cooking sequence with 100% reliability'
@@ -107,42 +119,42 @@ const projects = [
         stack: ['Java', 'Spring Boot', 'MongoDB', 'React.js', 'Bootstrap', 'Axios', 'Postman', 'IntelliJ', 'VS Code'],
         github: 'https://github.com/jaidensiu/Movie-Review-App'
     },
-    {
-        name: 'Travel Planner Mobile App',
-        description:
-            'This is a nwHacks 2023 hackathon project where my team and I built an Android mobile application to reduce the information overload experienced on travel sites. I was involved with implementing a REST API to save user profile data using Sequelize, MySQL, Express, and Node. I tested the API endpoints using MySQL Workbench and Postman to verify SQL CRUD operations. The UI/UX components were designed and emulated in Android Studio which was written in Java and XML.',
-        photo: null,
-        stack: ['Express.js', 'Node.js', 'JavaScript', 'Java', 'Android Studio', 'VS Code', 'MySQL', 'Postman'],
-        github: 'https://github.com/jaidensiu/nwHacks-2023',
-        livePreview: 'https://devpost.com/software/xplore-g1thmo'
-    },
-    {
-        name: 'Handwritten Digit Classifier GUI',
-        description:
-            'This project was inspired by a Nintendo DS game called Brain Age that I used to play as a child. In this game, one of the particular minigames is a timed challenge where you write out answers to 20 randomized arithmetic questions onto the DS console\'s touchscreen in sequence. To mimic the digit recognition system in the DS console, I decided to create a convolutional neural network using max pooling that would be trained with the MNIST dataset. To test the digit recognition system, I developed a Pygame module that would allow a user to write a digit for the system to recognize the digit.',
-        stack: ['Python', 'TensorFlow', 'Keras', 'Pygame', 'VS Code'],
-        github: 'https://github.com/jaidensiu/Digit-Recognition-System'
-    },
-    {
-        name: 'Rental Bike Usage Prediction',
-        description:
-            'This project involved applying machine learning and statistical models to forecast bike rental demand using weather, hourly rentals, and date data for bike rental supply stability. I conducted exploratory data analysis by visualizing the dataset via scatter plots to summarize the data and relation between different features. To apply models to the dataset, I employed a linear and mutiple linear regression model to best fit a line for the number of bikes dependent on temperature. I also used a vanilla neural network to fit a curve against the data. To evaluate the models, I used metrics such a mean squared error to quantify prediction accuracy. The findings of this project allows for ensuring a stable supply of rental bikes.',
-        stack: ['Python', 'NumPy', 'Pandas', 'Matplotlib', 'Seaborn', 'Scikit-learn', 'TensorFlow', 'Keras', 'Google Colab'],
-        sourceCode: 'https://colab.research.google.com/drive/17NUwQiYhrxbHLSbDXd86RwJlmRIa-9wn?usp=sharing'
-    },
-    {
-        name: 'Microfluidic Characterization Device',
-        description:
-            'A design course project inspired by cell sorting techniques in microfluidic devices. In a team, we designed an electromechanical device capable of sorting ping pong balls of two colours. We also produced design history files according to FDA guidelines that are used for medical device development. Overall, we modeled mechanical actuation using SolidWorks by finite element analysis, designed analog circuits to actuate device with photoresistor-integrated sensors, and wrote Arduino sketch software to control the actuation of physical components such as servo motors.',
-        stack: ['C++', 'Arduino', 'SolidWorks', 'MultiSim']
-    },
-    {
-        name: 'Autonomous Robotic Claw',
-        description:
-            'A design course project that introduces fundamental engineering design concepts from rapid-protyping to iterative design processes. I designed a pulley system that geometrically optimized the claw to open and close, wrote Arduino sketch software to automate the claw, pinned electronics such as a sonar sensor and servo motor, and modeled claw actuation in SolidWorks. My design of the cardboard robotic claw was able to sense and pick up items from almonds to soup cans over an obstacle. This project was a highlight of my first year of engineering at UBC.',
-        stack: ['C++', 'Arduino', 'SolidWorks'],
-        github: 'https://github.com/jaidensiu/Autonomous-Robotic-Claw'
-    }
+    // {
+    //     name: 'Travel Planner Mobile App',
+    //     description:
+    //         'This is a nwHacks 2023 hackathon project where my team and I built an Android mobile application to reduce the information overload experienced on travel sites. I was involved with implementing a REST API to save user profile data using Sequelize, MySQL, Express, and Node. I tested the API endpoints using MySQL Workbench and Postman to verify SQL CRUD operations. The UI/UX components were designed and emulated in Android Studio which was written in Java and XML.',
+    //     photo: null,
+    //     stack: ['Express.js', 'Node.js', 'JavaScript', 'Java', 'Android Studio', 'VS Code', 'MySQL', 'Postman'],
+    //     github: 'https://github.com/jaidensiu/nwHacks-2023',
+    //     livePreview: 'https://devpost.com/software/xplore-g1thmo'
+    // },
+    // {
+    //     name: 'Handwritten Digit Classifier GUI',
+    //     description:
+    //         'This project was inspired by a Nintendo DS game called Brain Age that I used to play as a child. In this game, one of the particular minigames is a timed challenge where you write out answers to 20 randomized arithmetic questions onto the DS console\'s touchscreen in sequence. To mimic the digit recognition system in the DS console, I decided to create a convolutional neural network using max pooling that would be trained with the MNIST dataset. To test the digit recognition system, I developed a Pygame module that would allow a user to write a digit for the system to recognize the digit.',
+    //     stack: ['Python', 'TensorFlow', 'Keras', 'Pygame', 'VS Code'],
+    //     github: 'https://github.com/jaidensiu/Digit-Recognition-System'
+    // },
+    // {
+    //     name: 'Rental Bike Usage Prediction',
+    //     description:
+    //         'This project involved applying machine learning and statistical models to forecast bike rental demand using weather, hourly rentals, and date data for bike rental supply stability. I conducted exploratory data analysis by visualizing the dataset via scatter plots to summarize the data and relation between different features. To apply models to the dataset, I employed a linear and mutiple linear regression model to best fit a line for the number of bikes dependent on temperature. I also used a vanilla neural network to fit a curve against the data. To evaluate the models, I used metrics such a mean squared error to quantify prediction accuracy. The findings of this project allows for ensuring a stable supply of rental bikes.',
+    //     stack: ['Python', 'NumPy', 'Pandas', 'Matplotlib', 'Seaborn', 'Scikit-learn', 'TensorFlow', 'Keras', 'Google Colab'],
+    //     sourceCode: 'https://colab.research.google.com/drive/17NUwQiYhrxbHLSbDXd86RwJlmRIa-9wn?usp=sharing'
+    // },
+    // {
+    //     name: 'Microfluidic Characterization Device',
+    //     description:
+    //         'A design course project inspired by cell sorting techniques in microfluidic devices. In a team, we designed an electromechanical device capable of sorting ping pong balls of two colours. We also produced design history files according to FDA guidelines that are used for medical device development. Overall, we modeled mechanical actuation using SolidWorks by finite element analysis, designed analog circuits to actuate device with photoresistor-integrated sensors, and wrote Arduino sketch software to control the actuation of physical components such as servo motors.',
+    //     stack: ['C++', 'Arduino', 'SolidWorks', 'MultiSim']
+    // },
+    // {
+    //     name: 'Autonomous Robotic Claw',
+    //     description:
+    //         'A design course project that introduces fundamental engineering design concepts from rapid-protyping to iterative design processes. I designed a pulley system that geometrically optimized the claw to open and close, wrote Arduino sketch software to automate the claw, pinned electronics such as a sonar sensor and servo motor, and modeled claw actuation in SolidWorks. My design of the cardboard robotic claw was able to sense and pick up items from almonds to soup cans over an obstacle. This project was a highlight of my first year of engineering at UBC.',
+    //     stack: ['C++', 'Arduino', 'SolidWorks'],
+    //     github: 'https://github.com/jaidensiu/Autonomous-Robotic-Claw'
+    // }
 ]
 
 const blogs = [
